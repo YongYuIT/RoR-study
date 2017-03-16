8c4cce97fb5815648f2b57609dce7deeb170ea3b
YongYuIT <3126446495@qq.com>
2017-03-16 00:16:57 -0700

    chat with different client finish

--- RoRChatTest/ServerSide/app/channels/special_link_channel.rb
+++ RoRChatTest/ServerSide/app/channels/special_link_channel.rb
@@ -1,8 +1,10 @@
+$users=[]
 class SpecialLinkChannel < ApplicationCable::Channel
   def subscribed
     logger.info "yuyong---->subscribed-->"+params.to_s
     @userID = Time.new.to_i.to_s
     stream_for @userID
+    $users.push(@userID)
   end
 
   def unsubscribed
@@ -11,6 +13,13 @@ class SpecialLinkChannel < ApplicationCable::Channel
 
   def s_say_hello(msg_from_client)
     logger.info "yuyong---->s_say_hello"
-    SpecialLinkChannel.broadcast_to(@userID,to_client_message: "this is a message from server (reponse to message from client \"#{msg_from_client['hello_msg_to_server']}\")")
+    to_user=[]
+    $users.each{
+      |item|
+      if(item!=@userID)
+        to_user.push(item)
+      end
+    }
+    SpecialLinkChannel.broadcast_to(to_user,to_client_message: "this is a message from server (reponse to message from client \"#{msg_from_client['hello_msg_to_server']}\")")
   end
 end
